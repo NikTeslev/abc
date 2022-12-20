f74348026d38e005d815dc00b52ae7430e15b357
NikolaTes <kteslev@mail.ru>
2022-12-20 10:10:46 +0300

    Добавил описание и картинки в README.

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -16,6 +16,7 @@ string[] SortingArray(string[] array)
 
         {
             array2[i] = array[i];
+            
         }
     }
     return array2;
@@ -25,15 +26,18 @@ string[] SortingArray(string[] array)
 
 void Printarray(string[] array)
 {
-  
+
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
         if (i < array.Length - 1) Console.Write($"{array[i]},");
-        else Console.Write("{array[i]");
+        else Console.Write($"{array[i]}");
     }
     Console.WriteLine("]");
 }
+
 Printarray(incoming);
+
 string[] outgoing = SortingArray(incoming);
+Console.WriteLine();
 Printarray(outgoing);
